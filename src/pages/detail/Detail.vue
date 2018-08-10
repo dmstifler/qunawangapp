b34ba7690c3c8273c06468d4c1bdce9a063da987
dmstifler <zalx@vip.qq.com>
2018-08-10 08:10:59 +0800

    banner finish

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <detail-banner></detail-banner>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner: DetailBanner
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
